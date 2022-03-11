50982cdbef2f6f71fe5e50e963d0330b6d86cb67
aksu <Scyzhe@hotmail.com>
2022-03-11 12:05:13 +0200

    test heroku

--- part3/01/src/index.js
+++ part3/01/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 
-
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -108,7 +108,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
